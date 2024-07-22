1593fd69912a5e5a2423022612c21daef8bb8151
AhsanAli-Soomro <ahsanalisoomro147@gmail.com>
2024-07-22 12:53:40 +0500

    update card

--- src/app/components/Cards.jsx
+++ src/app/components/Cards.jsx
@@ -73,7 +73,7 @@ function Cards() {
                                 <li key={idx} className="mb-1">- {feature}</li>
                             ))}
                         </ul>
-                        <a href="#" className="mt-3 text-orange-400 dark:text-white hover:text-blue-600 inline-flex items-center">Learn More
+                        <a href="/About" className="mt-3 text-orange-400 dark:text-white hover:text-blue-600 inline-flex items-center">Learn More
                             <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
                                 strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                 <path d="M5 12h14M12 5l7 7-7 7"></path>
