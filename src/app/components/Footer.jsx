0696e0e0d6cad25ca15da3c7ac4f733969442a25
AhsanAli-Soomro <ahsanalisoomro147@gmail.com>
2024-07-22 15:34:37 +0500

    footer links edit

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -10,16 +10,10 @@ function Footer() {
                     </span>
                     <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-white dark:text-gray-400 sm:mt-0">
                         <li>
-                            <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                            <a href="/About" class="hover:underline me-4 md:me-6">About</a>
                         </li>
                         <li>
-                            <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline">Contact</a>
+                            <a href="/Hire" class="hover:underline">Contact</a>
                         </li>
                     </ul>
                 </div>
