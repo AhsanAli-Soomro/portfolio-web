35b9245dbb91e49b908d3bdfe4404ebd3d13bc4a
AhsanAli-Soomro <ahsanalisoomro147@gmail.com>
2024-07-23 17:28:54 +0500

    update modele

--- src/app/components/ProjectModal.jsx
+++ src/app/components/ProjectModal.jsx
@@ -7,8 +7,8 @@ const ProjectModal = ({ project, onClose }) => {
     if (!project) return null;
 
     return (
-        <div className="fixed inset-0 mb-10 z-50 flex items-center justify-center bg-black bg-opacity-75 overflow-y-auto">
-            <div className="bg-black border pt-6 m-10 p-8 rounded-lg max-w-3xl w-full mx-4 my-10 relative overflow-y-auto max-h-screen text-white">
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75 overflow-y-auto">
+            <div className="bg-black border pt-6 p-8 rounded-lg max-w-3xl w-full mx-4 my-10 relative overflow-y-auto max-h-screen text-white">
                 <button
                     onClick={onClose}
                     className="absolute top-2 right-2 text-orange-400 rounded-full p-2 hover:text-red-600 transition-transform transform hover:scale-110"
@@ -16,10 +16,21 @@ const ProjectModal = ({ project, onClose }) => {
                     &times;
                 </button>
                 <h2 className="text-3xl font-bold mb-4 text-orange-400">{project.title}</h2>
-                <div className='flex gap-6'>
-
-                    <Image src={project.image} width={400} height={300} alt={project.title} className="mb-4 rounded transition-transform transform hover:scale-110" />
-                    <Image src={project.imageapp} width={200} height={300} alt={project.title} className="mb-4 rounded transition-transform transform hover:scale-110" />
+                <div className="flex flex-col md:flex-row gap-6">
+                    <Image
+                        src={project.image}
+                        width={400}
+                        height={300}
+                        alt={project.title}
+                        className="mb-4 rounded transition-transform transform hover:scale-110"
+                    />
+                    <Image
+                        src={project.imageapp}
+                        width={200}
+                        height={300}
+                        alt={project.title}
+                        className="mb-4 rounded transition-transform transform hover:scale-110"
+                    />
                 </div>
                 <p className="text-lg mb-4">{project.description}</p>
                 {project.features && (
@@ -32,18 +43,31 @@ const ProjectModal = ({ project, onClose }) => {
                         </ul>
                     </div>
                 )}
-                <div className="flex justify-between mt-4">
+                <div className="flex flex-col sm:flex-row justify-between mt-4">
                     <button
                         onClick={onClose}
-                        className="bg-orange-500 text-white py-2 px-4 rounded hover:bg-orange-600 transition-transform transform hover:scale-110"
+                        className="bg-orange-500 text-white py-2 px-4 rounded hover:bg-orange-600 transition-transform transform hover:scale-110 mb-2 sm:mb-0"
                     >
                         Close
                     </button>
-                    <Link href={project.link} className="mt-3 text-orange-400 dark:text-white hover:text-blue-600 inline-flex items-center">Visit Site
-                        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
-                            strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                            <path d="M5 12h14M12 5l7 7-7 7"></path>
-                        </svg>
+                    <Link href={project.link}>
+                        <div
+                            className="mt-3 text-orange-400 dark:text-white hover:text-blue-600 inline-flex items-center transition-transform transform hover:scale-110"
+                            target="_blank"
+                        >
+                            Visit Site
+                            <svg
+                                fill="none"
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                className="w-4 h-4 ml-2"
+                                viewBox="0 0 24 24"
+                            >
+                                <path d="M5 12h14M12 5l7 7-7 7"></path>
+                            </svg>
+                        </div>
                     </Link>
                 </div>
             </div>
