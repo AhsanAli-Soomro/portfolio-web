ea521bb80f2d1ac074a9d50c9d409d9bab322ac1
Ahsan Ali Soomro <149801757+AhsanAli-Soomro@users.noreply.github.com>
2024-12-19 12:59:16 +0500

    Update about page

--- src/app/components/AboutComponent.jsx
+++ src/app/components/AboutComponent.jsx
@@ -8,45 +8,62 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const About = () => {
     return (
-        <div className="relative p-4 flex flex-col bg-black bg-cover bg-center min-h-screen">
-            <div className="absolute inset-0 opacity-50"></div>
+        <div className="relative p-6 flex flex-col bg-white min-h-screen">
             <div className="container mx-auto px-6 py-24 relative z-10">
-                <h1 className="text-5xl h-20 sm:h-10  text-orange-400 mb-6">
+                <h1 className="text-5xl font-bold text-purple-700 mb-6">
                     <TypewriterText />
                 </h1>
-                <p className="text-xl text-white mb-6">
+                <p className="text-lg text-gray-600 mb-6">
                     I am Ahsan, a passionate web designer dedicated to creating visually stunning and user-friendly websites. With a keen eye for design and a commitment to innovation, I transform ideas into digital experiences that captivate and engage.
                 </p>
                 <div className="flex flex-col lg:flex-row justify-between items-start">
                     <div className="w-full lg:w-1/2">
-                        <h2 className="text-4xl text-orange-400 mb-4">My Expertise</h2>
-                        <p className="text-lg text-white mb-6">
+                        <h2 className="text-3xl font-semibold text-purple-700 mb-4">My Expertise</h2>
+                        <p className="text-base text-gray-600 mb-6">
                             From responsive design to UI/UX prototyping, I cover a wide range of web design aspects to ensure your website stands out. My process includes thorough research, detailed wireframing, and iterative testing to deliver the best results.
                         </p>
                     </div>
                 </div>
-                <div className="flex space-x-6 mt-6">
-                    <a href="https://www.linkedin.com/in/ahsanali-soomro" className="text-orange-400 hover:text-white transition duration-300">
+                <div className="flex space-x-4 mt-6">
+                    <a
+                        href="https://www.linkedin.com/in/ahsanali-soomro"
+                        className="text-purple-700 hover:text-purple-900 transition duration-300"
+                    >
                         <FontAwesomeIcon icon={faLinkedin} size="2x" />
                     </a>
-                    <a href="mailto:ahsanalisoomro147@gmail.com" className="text-orange-400 hover:text-white transition duration-300">
+                    <a
+                        href="mailto:ahsanalisoomro147@gmail.com"
+                        className="text-purple-700 hover:text-purple-900 transition duration-300"
+                    >
                         <FontAwesomeIcon icon={faEnvelope} size="2x" />
                     </a>
-                    <a href="https://www.facebook.com/ahsanali.king.92" className="text-orange-400 hover:text-white transition duration-300">
+                    <a
+                        href="https://www.facebook.com/ahsanali.king.92"
+                        className="text-purple-700 hover:text-purple-900 transition duration-300"
+                    >
                         <FontAwesomeIcon icon={faFacebook} size="2x" />
                     </a>
-                    <a href="https://www.instagram.com/soomroahsan_ali/" className="text-orange-400 hover:text-white transition duration-300">
+                    <a
+                        href="https://www.instagram.com/soomroahsan_ali/"
+                        className="text-purple-700 hover:text-purple-900 transition duration-300"
+                    >
                         <FontAwesomeIcon icon={faInstagram} size="2x" />
                     </a>
-                    <a href="http://www.twitter.com/AhsanAli3860" className="text-orange-400 hover:text-white transition duration-300">
+                    <a
+                        href="http://www.twitter.com/AhsanAli3860"
+                        className="text-purple-700 hover:text-purple-900 transition duration-300"
+                    >
                         <FontAwesomeIcon icon={faTwitter} size="2x" />
                     </a>
-                    <a href="https://www.github.com/AhsanAli-Soomro" className="text-orange-400 hover:text-white transition duration-300">
+                    <a
+                        href="https://www.github.com/AhsanAli-Soomro"
+                        className="text-purple-700 hover:text-purple-900 transition duration-300"
+                    >
                         <FontAwesomeIcon icon={faGithub} size="2x" />
                     </a>
                 </div>
             </div>
-            <div className='w-full'>
+            <div className="w-full mt-12">
                 <Cards />
             </div>
         </div>
