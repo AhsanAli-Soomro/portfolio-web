9d48ee71a1caca6fef943d3e2d9f35232e07d631
Ahsan Ali Soomro <149801757+AhsanAli-Soomro@users.noreply.github.com>
2024-12-31 12:22:03 +0500

    Fix: Updated react-quill and resolved dependencies

--- src/app/components/Cards.jsx
+++ src/app/components/Cards.jsx
@@ -1,130 +1,105 @@
 "use client";
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import { Autoplay, Navigation, Pagination } from "swiper";
 
 const Cards = () => {
-    const CardData = [
-        {
-            title: 'Responsive Design',
-            features: [
-                'Ensuring websites look great on all devices',
-                'Flexible grid layouts',
-                'Media queries for adaptability',
-                'Optimized images for faster loading',
-                'Touch-friendly navigation'
-            ]
-        },
-        {
-            title: 'UI/UX Prototyping',
-            features: [
-                'Creating interactive prototypes',
-                'User flow diagrams',
-                'Wireframing and mockups',
-                'Usability testing and feedback',
-                'Iterative design improvements'
-            ]
-        },
-        {
-            title: 'SEO Optimization',
-            features: [
-                'Keyword research and integration',
-                'Optimizing meta tags and descriptions',
-                'Ensuring fast load times',
-                'Creating SEO-friendly URLs',
-                'Improving site architecture'
-            ]
-        },
-        {
-            title: 'Custom Animations',
-            features: [
-                'Engaging CSS animations',
-                'Interactive JavaScript effects',
-                'Smooth transitions and hover effects',
-                'Animation libraries integration',
-                'Performance optimization for animations'
-            ]
-        },
-        {
-            title: 'Cross-Browser Compatibility',
-            features: [
-                'Testing across major browsers',
-                'Ensuring consistent design and functionality',
-                'Polyfills for unsupported features',
-                'Debugging browser-specific issues',
-                'Maintaining legacy browser support'
-            ]
-        }
-    ];
+  const CardData = [
+    {
+      title: "Responsive Design",
+      features: [
+        "Ensuring websites look great on all devices",
+        "Flexible grid layouts",
+        "Media queries for adaptability",
+        "Optimized images for faster loading",
+        "Touch-friendly navigation",
+      ],
+    },
+    {
+      title: "UI/UX Prototyping",
+      features: [
+        "Creating interactive prototypes",
+        "User flow diagrams",
+        "Wireframing and mockups",
+        "Usability testing and feedback",
+        "Iterative design improvements",
+      ],
+    },
+    {
+      title: "SEO Optimization",
+      features: [
+        "Keyword research and integration",
+        "Optimizing meta tags and descriptions",
+        "Ensuring fast load times",
+        "Creating SEO-friendly URLs",
+        "Improving site architecture",
+      ],
+    },
+    {
+      title: "Custom Animations",
+      features: [
+        "Engaging CSS animations",
+        "Interactive JavaScript effects",
+        "Smooth transitions and hover effects",
+        "Animation libraries integration",
+        "Performance optimization for animations",
+      ],
+    },
+    {
+      title: "Cross-Browser Compatibility",
+      features: [
+        "Testing across major browsers",
+        "Ensuring consistent design and functionality",
+        "Polyfills for unsupported features",
+        "Debugging browser-specific issues",
+        "Maintaining legacy browser support",
+      ],
+    },
+  ];
 
-    return (
-        <div className="bg-white py-12">
-            <div className="container mx-auto">
-            <h2 className="text-4xl font-extrabold text-purple-700 mb-8 text-center">
-                Services
-            </h2>
-            <Swiper
-                modules={[Navigation, Pagination]}
-                navigation
-                pagination={{ clickable: true }}
-                loop
-                centeredSlides={true}
-                slidesPerView={1.2}
-                spaceBetween={30}
-                breakpoints={{
-                    768: {
-                        slidesPerView: 2.2,
-                    },
-                    1024: {
-                        slidesPerView: 3.2,
-                    },
-                }}
-                className="relative "
+  return (
+    <div className="py-12">
+      <div className="container mx-auto px-6">
+        <h2 className="text-4xl font-extrabold text-orange-800 mb-12 text-center">
+          Services
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {CardData.map((data, index) => (
+            <div
+              key={index}
+              className="relative flex flex-col justify-between bg-white rounded-xl shadow-lg p-6 border border-gray-200 transform transition-transform hover:scale-105"
             >
-                {CardData.map((data, index) => (
-                    <SwiperSlide key={index}>
-                        <div className="py-10">
-
-                        <div className="relative flex flex-col justify-center bg-white rounded-xl shadow-lg p-6 border border-gray-200 transform transition-transform hover:scale-105">
-                            <h3 className="text-lg font-semibold text-gray-800 mb-4">
-                                {data.title}
-                            </h3>
-                            <ul className="text-sm leading-relaxed text-gray-600">
-                                {data.features.map((feature, idx) => (
-                                    <li key={idx} className="mb-2">
-                                        - {feature}
-                                    </li>
-                                ))}
-                            </ul>
-                            <a
-                                href="/About"
-                                className="mt-auto text-purple-700 hover:text-purple-900 font-medium transition duration-300 inline-flex items-center"
-                            >
-                                Learn More
-                                <svg
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    className="w-4 h-4 ml-2"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                                </svg>
-                            </a>
-                        </div>
-                        </div>
-                    </SwiperSlide>
+              <h3 className="text-lg font-semibold text-orange-800 mb-4">
+                {data.title}
+              </h3>
+              <ul className="text-sm leading-relaxed text-gray-600 mb-4">
+                {data.features.map((feature, idx) => (
+                  <li key={idx} className="mb-2">
+                    - {feature}
+                  </li>
                 ))}
-            </Swiper>
-
+              </ul>
+              <a
+                href="/About"
+                className="text-orange-700 hover:text-orange-900 font-medium transition duration-300 inline-flex items-center"
+              >
+                Learn More
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
             </div>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,44 +1,44 @@
 "use client";
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="fixed py-2 bg-gray-100 top-0 w-full z-50">
-      <div className="container mx-auto px-6 py-0 flex justify-between items-center">
+    <nav className="fixed py-4 bg-white top-0 w-full z-50 shadow-lg">
+      <div className="container mx-auto px-6 flex justify-between items-center">
         {/* Logo */}
-        <a href="/" className="flex items-center text-purple-700 font-bold text-2xl">
-          <span className='text-4xl'>ahsan.</span>
+        <a href="/" className="flex items-center text-orange-800 font-bold text-2xl">
+          <span className="text-4xl">ahsan.</span>
         </a>
 
         {/* Navigation Links */}
-        <div className="hidden md:flex space-x-8 text-gray-600">
+        <div className="hidden md:flex space-x-8 text-orange-800 font-medium">
           <a
             href="/"
-            className="hover:text-purple-700 transition duration-300"
+            className="hover:text-yellow-300 transition duration-300"
           >
             Home
           </a>
           <a
             href="/About"
-            className="hover:text-purple-700 transition duration-300"
+            className="hover:text-yellow-300 transition duration-300"
           >
             About
           </a>
           <a
             href="/Projects"
-            className="hover:text-purple-700 transition duration-300"
+            className="hover:text-yellow-300 transition duration-300"
           >
             Projects
           </a>
           <a
             href="/Blogs"
-            className="hover:text-purple-700 transition duration-300"
+            className="hover:text-yellow-300 transition duration-300"
           >
             Blogs
           </a>
           <a
             href="/Resume"
-            className="hover:text-purple-700 transition duration-300"
+            className="hover:text-yellow-300 transition duration-300"
           >
             Resume
           </a>
@@ -47,7 +47,7 @@ const Navbar = () => {
         {/* Call to Action Button */}
         <a
           href="/Hire"
-          className="hidden md:inline-block bg-purple-700 text-white px-4 py-2 rounded-md hover:bg-purple-900 transition duration-300"
+          className="hidden md:inline-block bg-gradient-to-r from-orange-800 via-orange-600 to-orange-500 text-white px-4 py-2 rounded-md font-medium hover:bg-yellow-300 hover:text-orange-800 transition duration-300 shadow-md"
         >
           Let’s Talk
         </a>
@@ -55,8 +55,8 @@ const Navbar = () => {
         {/* Mobile Menu Button */}
         <button
           type="button"
-          className="md:hidden text-purple-700 focus:outline-none"
-          onClick={() => alert('Open mobile menu!')}
+          className="md:hidden text-orange-800 focus:outline-none"
+          onClick={() => alert("Open mobile menu!")}
         >
           <svg
             className="w-6 h-6"
